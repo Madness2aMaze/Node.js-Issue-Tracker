47b894210130118829153e2f8964d38cfd845646
Cătălin Anghel-Ursu <cat.ang.dev@gmail.com>
2019-06-05 23:50:29 +0300

    Update api.js
    Updated the .get() method logic to allow for query string filtering based on the req.query obj. Optimized the .post() method with a standardIssue object as a model for the new issues. Replaced res.json() method with .send() to make the assertions from the tests work as expected. Written the .put() method and works as it should when updating or closing an issue. Once closed an issue cannot be opened, just deteled. All 11 functional tests are passing. The project is now complete and ready to be submitted to FCC for review.

--- routes/api.js
+++ routes/api.js
@@ -8,18 +8,29 @@
 
 'use strict';
 
-const expect      = require('chai').expect;
-const ObjectId    = require('mongodb').ObjectID;
+const expect   = require('chai').expect;
+const ObjectId = require('mongodb').ObjectID;
+
 
 module.exports = (app, db) => {
   
   app.route('/api/issues/:project')
     .get((req, res) => {
-    const project = req.params.project;
-    db.collection(project).find({}).toArray((err, issues) => {
-      err ? res.json(err) : res.json(issues);
-      //console.log(issues);
-      db.close();
+    const project = req.params.project;        
+    const issueQuery = req.query; //getting the query object filled with the query strings   
+    
+    issueQuery._id ? issueQuery._id = new ObjectId(issueQuery._id) : undefined;
+    
+    //assigning the boolean value resulting from the comparison to the issueQuery.open property before running the .find() method
+    issueQuery.open ? issueQuery.open = issueQuery.open.toString() == "true" : null; 
+    
+    //running the .find() method with the query object req.query resulted from the user query strings
+    //on the first run the query object is empty and the .find() returns all the issue entries for that particular project
+    db.collection(project).find(issueQuery).toArray((err, issues) => {      
+      err ? res.send(err) : res.send(issues);
+      
+      //console.log(issueQuery);
+      //db.close();
     });
   })
   
@@ -29,85 +40,85 @@ module.exports = (app, db) => {
     console.log(project);
     
     const issueTitle = req.body.issue_title;
-    const issueText = req.body.issue_text;
-    const submitter = req.body.created_by;
+    const issueText  = req.body.issue_text;
+    const createdBy  = req.body.created_by;
+    const assignedTo = req.body.assigned_to;
+    const statusText = req.body.status_text;
     
-    if (!issueTitle) { //checking to see if the user entered a title in the input field
-      res.json('Please enter a title for your issue');
-    } else if (!issueText) { //checking to see if the user entered a description in the input field
-      res.json('Please enter a description for your issue');
-    } else if (!submitter) { //checking to see if the user entered his name in the input field
-      res.json('Please enter your name');
+    const standardIssue = {
+      issue_title: issueTitle,
+      issue_text:  issueText,
+      created_on:  new Date().toISOString(),
+      updated_on:  new Date().toISOString(),
+      created_by:  createdBy,
+      assigned_to: assignedTo || "",
+      open:        true,
+      status_text: statusText || ""
+    }  
+    
+    if (!issueTitle || !issueText || !createdBy ) { //checking to see if the user entered a title, description and name in the required input fields
+      res.send('*required fields missing');
     } else { //all fields are filled => proceed to add the issue to the database
-      db.collection(project).insertOne({
-        "issue_title": req.body.issue_title,
-        "issue_text": req.body.issue_text,
-        "created_on": new Date().toISOString(),
-        "updated_on": new Date().toISOString(),
-        "created_by": req.body.created_by,
-        "assigned_to": req.body.assigned_to,
-        "open": true,
-        "status_text": req.body.status_text
-      }, (err, issue) => {
-        console.log('Issue ' + req.body.issue_title + ' has been successfully submitted.');
-        res.json(issue.ops);
-        if (err) {
-          res.redirect('/');
-        } else {
-          res.json(null, issue);
-        }
-        db.close();
+      db.collection(project).insertOne(standardIssue, (err, issue) => {
+        //console.log('Issue ' + issueTitle + ' has been successfully submitted.');
+        standardIssue._id = issue.insertedId;
+        //console.log(issue); 
+        err ? res.send(err) : res.json(standardIssue);
+        //db.close();
       })
     }
   })
   
   //updating the issue
     .put((req, res) => {
-    const project = req.params.project;
+    const project = req.params.project;    
+    const issueUpdate = req.body; // issueUpdate obj composed of the form fields
     
-    /*db.collection(project).findOne({
-      id: req.body._id
-    }, (err, issue) => {
-      if (err) {
-        next(err);
-      } else if (issue) {
-        res.redirect('/');
-      } else {
-        db.collection(project).insertOne({
-          "issue_title": req.body.issue_title,
-          "issue_text": req.body.issue_text,
-          "created_on": new Date().toISOString(),
-          "updated_on": new Date().toISOString(),
-          "created_by": req.body.created_by,
-          "assigned_to": req.body.assigned_to,
-          "open": true,
-          "status_text": req.body.status_text
-        }, (err, doc) => {
-          console.log('Issue ' + doc._id + ' has been successfully submitted.');
-          res.json(doc);
-          if (err) {
-            res.redirect('/');
-          } else {
-            next(null, doc);
-          }
-        })
-      }
-    })*/
-  })
+    const issueTitle  = req.body.issue_title;
+    const issueText   = req.body.issue_text;
+    const createdBy   = req.body.created_by;
+    const assignedTo  = req.body.assigned_to;
+    const statusText  = req.body.status_text;
+    const issueId     = req.body._id;    
+    
+    // a series of checks to refine the updates object and keep only the user completed form fields(updates) before is sent to the update method      
+    !issueTitle ? delete issueUpdate.issue_title : null; 
+    !issueText  ? delete issueUpdate.issue_text  : null; 
+    !createdBy  ? delete issueUpdate.created_by  : null; 
+    !assignedTo ? delete issueUpdate.assigned_to : null; 
+    !statusText ? delete issueUpdate.status_text : null; 
+                  delete req.body._id; //reseting the body._id from memory          
+    
+    //assigning the boolean value resulting from the comparison to the standardUpdates.open property before running the .find() method
+    issueUpdate.open ? issueUpdate.open = issueUpdate.open.toString() == "true" : null; 
+    
+    //console.log(Object.entries(issueUpdate));
+    
+    if (Object.entries(issueUpdate).length === 0) { // form input fields are empty
+      res.send('no updated field sent');
+    } else { //all required fields are filled => proceed to find the issue by the _id provided and update it's entry in the database
+      issueUpdate.updated_on =  new Date().toISOString(); //reset the updated_on property
+      db.collection(project).findOneAndUpdate({_id: ObjectId(issueId)}, { $set: issueUpdate }, (err, issue) => {
+        err ? res.send('could not update ' + issueId) : res.send('successfully updated');        
+        //console.log(req.body);
+        //db.close();
+      })
+    }
+  })  
   
   //deleting the issue
-    .delete(function(req, res) {
+    .delete((req, res) => {
     const project = req.params.project;
     const issueId = req.body._id;
     
-    if (!issueId) { //checking to see if the user entered issue _id in the input field
-      res.json('_id error');
+    if (!issueId) { //checking to see if the user entered issue's _id in the input field
+      res.send('_id error');
     } else { //_id entered=> proceed to delete the issue from the database
-      db.collection(project).deleteOne({_id: ObjectId(issueId)}, (err, obj) => {
-      err ? res.json("could not delete " + issueId) : res.json("deleted: " + issueId);
-      //console.log(issues);
-      db.close();
-    });
+      db.collection(project).deleteOne({_id: ObjectId(issueId)}, (err, issue) => {
+        err ? res.send('could not delete ' + issueId) : res.send('deleted ' + issueId);
+        //console.log(issues);
+        //db.close();
+      });
     }
   });
     
